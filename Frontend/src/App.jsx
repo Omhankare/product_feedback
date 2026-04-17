bf3e9527e065752c92bdde545b965329a36742a4
omHankare <omhankare29@gmail.com>
2026-04-17 20:46:46 +0530

    react-routes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import AdminDashboard from "./Components/AdminDashboard";
 import Feedback from "./Components/Feedback";
 import Login from "./Components/Login";
@@ -7,10 +9,12 @@ import Register from "./Components/Register";
 function App() {
   return (
     <>
-      <Login />
-      <Register />
-      {/* <Feedback /> */}
-      {/* <AdminDashboard /> */}
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/feedback" element={<Feedback />}></Route>
+        <Route path="/adminDashboard" element={<AdminDashboard />}></Route>
+      </Routes>
     </>
   );
 }
